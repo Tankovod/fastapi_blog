655c24ace4e841adea676fef3c59ebe403a08a71
Tankovod <Wotdimas@gmail.com>
2024-01-14 21:09:52 +0300

    simple push -_-

--- src/cv/endpoints.py
+++ src/cv/endpoints.py
@@ -1,6 +1,7 @@
 from .router import router
 from src.settings import templates
 from fastapi import Request, status
+from fastapi.responses import FileResponse
 
 
 @router.get(
@@ -13,9 +14,9 @@ async def dmitry_cv(request: Request):
 
 
 @router.get(
-    "dmitry-belousov-resume/",
+    "/dmitry-belousov-cv.pdf",
     status_code=status.HTTP_200_OK,
     name="Резюме Дмитрия Pdf"
 )
-async def dmitry_cv(request: Request):
-    return templates.TemplateResponse(name="cv/dmitry-belousov-resume", context={"request": request, "status": 200})
+async def dmitry_cv_pdf(request: Request):
+    return FileResponse("static/cv/dmitry-belousov-cv.pdf", media_type="application/pdf", headers=request.headers)
